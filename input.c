7993232f166c07b57db61c1a18d06319429d063a
MannShah0312 <134050095+MannShah0312@users.noreply.github.com>
2025-09-04 01:59:51 +0530

    Adding example input and output files

---
+++ input.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int a = 3;
+    int b = 4;
+    int c;
+    c = a + b;
+    if (c < 5){
+        c = 10;
+    }
+    else {
+        c = 100;
+    }
+    
+    return 0;
+}
